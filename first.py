fa349b11709616e4cd73164680838c1887a6460b
Asmita Ojha <asmitaojha05@gmail.com>
2024-02-07 12:12:51 +0545

    added in different line than in main

--- first.py
+++ first.py
@@ -2,6 +2,11 @@ para= "Snake"
 graph = "group"
 print(para,graph)
 print("This is a new feature") 
+
+
+
+
+print("added in different line")
 print("I am adding a new feature in my repo")
 fruits = ['apple','mango','pomegranate','graphes']
 print(fruits)
