81a0cb1365e06d12c3c3ca7ad6f28e922a6035c6
Asmita Ojha <asmitaojha05@gmail.com>
2024-02-07 12:15:08 +0545

    changed

--- first.py
+++ first.py
@@ -2,10 +2,6 @@ para= "Snake"
 graph = "group"
 print(para,graph)
 print("This is a new feature") 
-
-
-
-
 print("added in different line")
 print("I am adding a new feature in my repo")
 fruits = ['apple','mango','pomegranate','graphes']
